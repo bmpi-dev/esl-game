7c695419c30bbfc3b84558caf735de9897b83d6e
Dawei Ma <madawei2699@gmail.com>
2021-09-17 22:53:48 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import qs from './q.json';
+	let q = qs[Math.floor(Math.random()*qs.length)];
+</script>
+
+<main class="bg-gray-200 p-8 m-8 rounded-lg">
+	<p>{q.question}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
