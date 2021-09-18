91cafa26966647830953dec25aa91d787df243ad
Dawei Ma <madawei2699@gmail.com>
2021-09-18 14:40:24 +0800

    add shuffle question list

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,28 @@
 <script>
 	import qs from './q.json';
+
+	function shuffle(array) {
+		let currentIndex = array.length,  randomIndex;
+
+		// While there remain elements to shuffle...
+		while (currentIndex != 0) {
+
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+
+			// And swap it with the current element.
+			[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+		}
+
+		return array;
+	}
+
+	shuffle(qs);
+
 	let q = qs[Math.floor(Math.random()*qs.length)];
+
 </script>
 
 <main class="bg-gray-200 p-8 m-8 rounded-lg">
